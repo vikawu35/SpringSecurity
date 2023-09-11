0c24789d996834b38c2628d27acd73de03a53650
vikawu35 <122668662+vikawu35@users.noreply.github.com>
2023-09-11 13:52:32 +0900

    Update UserRepository.java

--- project-prerequisite-3-1-2-main/src/main/java/habsida/spring/boot_security/demo/repository/UserRepository.java
+++ project-prerequisite-3-1-2-main/src/main/java/habsida/spring/boot_security/demo/repository/UserRepository.java
@@ -5,7 +5,6 @@ import habsida.spring.boot_security.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
